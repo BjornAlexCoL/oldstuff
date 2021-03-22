12ba74551a8a82af9c0eddf873bf5f4118ed454e
BjornAlexCoL <75667280+BjornAlexCoL@users.noreply.github.com>
2021-03-22 11:02:33 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/java/g34/oldstuff/OldstuffApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.java.g34.oldstuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldstuffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OldstuffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/java/g34/oldstuff/OldstuffApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.java.g34.oldstuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OldstuffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
